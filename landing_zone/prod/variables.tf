ee357e74b84c8f73067aaa79d93e0adcac6a8766
Ankur Srivastava <ankur.ds.aws@gmail.com>
2025-07-02 10:51:19 +0530

    prod-pipeline-testing

--- landing_zone/prod/variables.tf
+++ landing_zone/prod/variables.tf
@@ -19,9 +19,29 @@ variable "tags" {
   type = map(string)
 }
 
+variable "security_groups" {
+  description = "Map of security groups"
+  type = map(object({
+    ingress_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+    egress_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+  }))
+}
 
+variable "vpc_id" {
+  description = "VPC ID where security groups will be created"
+  type        = string
+}
 
-variable "security_groups" {}
 
 
 variable "keypair_name" {
--- module/security_group/outputs.tf
+++ module/security_group/outputs.tf
@@ -1,3 +1,6 @@
 output "security_group_ids" {
-  value = { for sg_name, sg in aws_security_group.this : sg_name => sg.id }
+  value = {
+    for sg_name, sg in aws_security_group.this :
+    sg_name => sg.id
+  }
 }
