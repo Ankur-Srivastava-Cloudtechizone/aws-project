de712b5309cbc0c22281df349505c87d89a1f1c7
Ankur Srivastava <ankur.ds.aws@gmail.com>
2025-07-02 15:33:24 +0530

    prod-pipeline-testing

--- module/vpc/outputs.tf
+++ module/vpc/outputs.tf
@@ -2,6 +2,12 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
+output "private_subnet_ids" {
+  value = {
+    for key, subnet in aws_subnet.private : key => subnet.id
+  }
+}
+
 output "route_table_id" {
   value = aws_route_table.this.id
 }
