36f98af536ea17a4a3c4c26674477ec82f2383f9
Ankur Srivastava <ankur.ds.aws@gmail.com>
2025-07-02 16:11:38 +0530

    prod-pipeline-testing

--- module/ec2/variables.tf
+++ module/ec2/variables.tf
@@ -1,19 +1,14 @@
-variable "instances" {
-  description = "Map of EC2 instances to launch"
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "ec2_instances" {
   type = map(object({
-    ami_id        = string
-    instance_type = string
-    key_name      = string
-    instance_name = string
+    ami                  = string
+    instance_type        = string
+    subnet_id            = string
+    key_name             = string
+    security_group_ids   = list(string)
   }))
-}
-
-variable "subnet_id" {
-  description = "ID of the subnet to launch the EC2 instance in"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "ID of the security group to attach to the EC2 instance"
-  type        = string
 }
